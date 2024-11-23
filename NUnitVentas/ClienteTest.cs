de3216d10bd4fdf693fa9999abec3155a2534199
Marlon Parra <marlon1428@gmail.com>
2024-11-23 09:18:50 -0500

    primer commit

---
+++ NUnitVentas/ClienteTest.cs
@@ -0,0 +1,38 @@
+﻿using CapaModelo;
+using CapaDatos;
+
+namespace NUnitVentas
+{
+    [TestFixture]
+    public  class ClienteTest
+    {
+
+        public Cliente nuevoCliente = null;
+    
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Cliente_RegistrarCliente_ReturnTrue()
+        {
+              nuevoCliente = new Cliente {
+                IdCliente=0,
+                TipoDocumento = "DNI",
+                NumeroDocumento = "47151218",
+                Nombre = "Jorge Muñoz",
+                Direccion = "Prol. Huascar",
+                Telefono = "981566223"
+                //Activo = true
+            };
+
+            var resultado = CD_Cliente.Instancia.RegistrarCliente(nuevoCliente);
+
+           
+            // Afirmar: Comprobamos que el resultado sea verdadero, indicando que el registro fue exitoso
+            Assert.IsTrue(resultado, "El cliente debería haberse registrado correctamente en la base de datos.");
+        }
+
+    }
+}
---
+++ NUnitVentas/ProductosTest.cs
@@ -0,0 +1,44 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitVentas
+{
+
+    [TestFixture]
+    public  class ProductosTest
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Producto_EliminarProducto_ReturnTrue()
+        {
+            int idProducto = 6;
+
+            var resultado = CD_Producto.Instancia.EliminarProducto(idProducto);
+
+            // Afirmar: Comprobamos que el resultado sea verdadero, indicando que el registro fue exitoso
+            Assert.True(resultado, "Producto eliminado correctamente");
+
+            if (resultado)
+            {
+                TestContext.WriteLine("La prueba fue exitosa: El producto se correctamente.");
+            }
+            else
+            {
+                TestContext.WriteLine("La prueba falló: No se pudo eliminar el producto.");
+            }
+
+        }
+
+
+    }
+}
